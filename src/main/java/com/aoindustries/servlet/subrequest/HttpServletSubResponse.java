6889aaad1aea6a0374768b72c6dba4a040bbad24
AO Industries, Inc. <support@aoindustries.com>
2020-02-03 04:25:27 -0600

    Using ao-fluent-html for <meta> tags.

--- src/main/java/com/aoindustries/servlet/subrequest/HttpServletSubResponse.java
+++ src/main/java/com/aoindustries/servlet/subrequest/HttpServletSubResponse.java
@@ -1,6 +1,6 @@
 /*
  * ao-servlet-subrequest - Servlet sub-request wrappers with optional concurrency.
- * Copyright (C) 2016, 2017, 2019  AO Industries, Inc.
+ * Copyright (C) 2016, 2017, 2019, 2020  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -49,7 +49,7 @@ public class HttpServletSubResponse extends ServletSubResponse implements IHttpS
 
 	@Override
 	public void setContentLength(int len) {
-		setIntHeader("Content-Length", len);
+		setIntHeader("content-length", len);
 	}
 
 	/**
--- src/main/java/com/aoindustries/servlet/subrequest/HttpServletSubResponseWrapper.java
+++ src/main/java/com/aoindustries/servlet/subrequest/HttpServletSubResponseWrapper.java
@@ -1,6 +1,6 @@
 /*
  * ao-servlet-subrequest - Servlet sub-request wrappers with optional concurrency.
- * Copyright (C) 2016, 2017, 2019  AO Industries, Inc.
+ * Copyright (C) 2016, 2017, 2019, 2020  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -56,7 +56,7 @@ public class HttpServletSubResponseWrapper extends ServletSubResponseWrapper imp
 
 	@Override
 	public void setContentLength(int len) {
-		setIntHeader("Content-Length", len);
+		setIntHeader("content-length", len);
 	}
 
 	/**
