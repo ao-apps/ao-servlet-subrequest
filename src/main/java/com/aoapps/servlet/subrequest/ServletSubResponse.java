d84b1afd3a0051a6bb17c57b773a1174fbbeaa5a
AO Industries, Inc <support@aoindustries.com>
2022-04-16 01:49:27 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/aoapps/servlet/subrequest/ServletSubResponse.java
+++ src/main/java/com/aoapps/servlet/subrequest/ServletSubResponse.java
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with ao-servlet-subrequest.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.aoapps.servlet.subrequest;
 
 import com.aoapps.io.buffer.AutoTempFileWriter;
